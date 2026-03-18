d9fed8015b1a9810be1454615d3a284ed0cadf61
Namchheav009 <namchheavron.info@gmail.com>
2026-03-18 16:48:29 +0700

    update budget page and update readme

--- resources/js/Pages/Budgets.tsx
+++ resources/js/Pages/Budgets.tsx
@@ -186,17 +186,7 @@ export function Budgets({
                   </option>
                 ))}
               </select>
-              <select
-                value={selectedUserId}
-                onChange={(e) => setSelectedUserId(e.target.value)}
-                className="px-3 py-2 border border-slate-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:border-transparent bg-white"
-              >
-                {users.map((u) => (
-                  <option key={u.id} value={u.id}>
-                    {u.username || u.name}
-                  </option>
-                ))}
-              </select>
+              
             </>
           )}
           <button
