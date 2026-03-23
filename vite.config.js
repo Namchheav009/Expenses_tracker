099f61239759c20963226df8f396d924702787ef
Namchheav009 <namchheavron.info@gmail.com>
2026-03-23 16:27:43 +0700

    update for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.tsx',
-                'resources/js/App.tsx',
-            ],
+            input: 'resources/js/App.tsx',
             refresh: true,
         }),
         react(),
